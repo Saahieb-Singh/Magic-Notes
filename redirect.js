22456ee6ea1589226247076a24496adcfbe7c403
Saahieb-Singh <85167617+Saahieb-Singh@users.noreply.github.com>
2026-08-30 17:06:55 +0530

    Update redirect target URL in handler function

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  const target = "http://localhost:5000/hackaicon_ethiack_1337_lmao";
+  const target = "http://localhost:5000/ethiack_1337_lmao";
 
   res.writeHead(302, { Location: target });
   res.end();
